1e263d8fec607644e7c89874c48d6f0d5cc7ab71
Harshith N J <njharshith122@gmail.com>
2025-01-15 18:31:24 +0530

    Started the project

---
+++ src/test/java/org/meditation/meditation_tracker/MeditationTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.meditation.meditation_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeditationTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
